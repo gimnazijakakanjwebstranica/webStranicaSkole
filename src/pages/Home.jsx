7a538414410de2a3803496f297b1480c2c4359e4
Nedim Neimarlija <nedimnneimarlija@gmail.com>
2023-12-01 23:30:26 +0100

    drko sam malo footer, sutra cu ga jos preraditi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
 
 const Home = () => {
     return (
         <div className="font-link">
             <NavBar/>
             <h1>Home</h1>
+            <Footer/>
         </div>
     )
 }
