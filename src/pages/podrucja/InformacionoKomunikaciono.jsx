ac10df8443e0b87ca2c6198be86986dcf165f6c1
Nedim Neimarlija <nedimnneimarlija@gmail.com>
2023-12-20 23:59:09 +0100

    footer popravio i dodao za IK predmete,takodje pokusao nesto sa  resting APIs kod profeora ali nije islo pa izbrisao, footer cu doraditi jos malo dalje i dodati predmete za ostala podrucja kao i sve fiksne stvari

--- src/pages/podrucja/InformacionoKomunikaciono.jsx
+++ src/pages/podrucja/InformacionoKomunikaciono.jsx
@@ -1,11 +1,320 @@
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+
 const InformacionoKomunikaciono = () => {
-  return <div>
-    <NavBar />
-  
-    <Footer/>
-  </div>;
+  return (
+    <div>
+      <NavBar />
+      <div className="container mx-auto mt-8">
+        <h2 className="text-2xl font-semibold mb-4">III RAZRED</h2>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Bosanski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Engleski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Njemacki Jezik/Turski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Latinski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Tjelesni i zdravstveni odgoj</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Vjeronauka</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Sociologija</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Psihologija</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Demkokratija</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Programiranje</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Operativni sistemi</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Web Dizajn</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Sigurnost informacija</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Baze podataka</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Matematika</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+
+      <div className="container mx-auto mt-8">
+        <h2 className="text-2xl font-semibold mb-4">IV RAZRED</h2>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Bosanski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Engleski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Njemacki Jezik/Turski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Latinski Jezik</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Tjelesni i zdravstveni odgoj</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Vjeronauka</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Logika sa filozofijom</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Kultura religija</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Izborni predmet</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Programiranje</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Razvoj mobilnih aplikacija</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Web Dizajn</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Racunarske mreze</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Baze podataka</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+
+        <div className="mb-8">
+          <table className="min-w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border p-3 text-left font-semibold">Matematika</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default InformacionoKomunikaciono;
--- src/pages/uposlenici/Profesori.jsx
+++ src/pages/uposlenici/Profesori.jsx
@@ -1,11 +1,104 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+
 const Profesori = () => {
-  return <div>
-    <NavBar />
-  
-    <Footer/>
-  </div>;
+  const [profesori, setProfesori] = useState([]);
+  const [ime, setIme] = useState("");
+  const [prezime, setPrezime] = useState("");
+
+  useEffect(() => {
+    // Dobavljanje profesora s backenda
+    fetchData();
+  }, []); // Ispravljena zagrada
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/api/profesori");
+      setProfesori(response.data);
+    } catch (error) {
+      console.error("Greška pri dobavljanju profesora", error);
+    }
+  };
+
+  const dodajProfesora = async () => {
+    try {
+      await axios.post("/api/profesori", { ime, prezime });
+      setIme("");
+      setPrezime("");
+      fetchData();
+    } catch (error) {
+      console.error("Greška pri dodavanju profesora", error);
+    }
+  };
+
+  const obrisiProfesora = async (id) => {
+    try {
+      await axios.delete(`/api/profesori/${id}`);
+      fetchData();
+    } catch (error) {
+      console.error("Greška pri brisanju profesora", error);
+    }
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <div className="container mx-auto mt-8">
+        <h2 className="text-2xl font-semibold mb-4">Profesori</h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            className="w-1/4 p-2 border border-gray-300 mr-2 rounded-l"
+            placeholder="Ime"
+            value={ime}
+            onChange={(e) => setIme(e.target.value)}
+          />
+          <input
+            type="text"
+            className="w-1/4 p-2 border border-gray-300 mr-2 rounded-l"
+            placeholder="Prezime"
+            value={prezime}
+            onChange={(e) => setPrezime(e.target.value)}
+          />
+          <button
+            className="w-1/4 bg-blue-500 text-white p-2 rounded-r"
+            onClick={dodajProfesora}
+          >
+            Dodaj profesora
+          </button>
+        </div>
+
+        <table className="min-w-full table-auto">
+          <thead>
+            <tr>
+              <th className="border p-3">Ime</th>
+              <th className="border p-3">Prezime</th>
+              <th className="border p-3">Akcije</th>
+            </tr>
+          </thead>
+          <tbody>
+            {profesori.map((profesor) => (
+              <tr key={profesor.id}>
+                <td className="border p-3">{profesor.ime}</td>
+                <td className="border p-3">{profesor.prezime}</td>
+                <td className="border p-3">
+                  <button
+                    className="bg-red-500 text-white p-2 rounded"
+                    onClick={() => obrisiProfesora(profesor.id)}
+                  >
+                    Obriši
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Profesori;
