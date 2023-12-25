cbdfa95eb7c951786d86e990461f83299955f2ee
mirzareact <hodzic04mirza@gmail.com>
2023-12-25 22:30:20 +0100

    probavam v3

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://gimnazija.netlify.app/.netlify/functions/api"
+    "https://gimnazija.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
