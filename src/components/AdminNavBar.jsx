a4b4b03b589c4eeb9204273dbf3af6c8b5d3fc46
mirzareact <hodzic04mirza@gmail.com>
2023-12-30 19:05:22 +0100

    malo doradio admin navBar, sve je ostalo na nedimu

--- src/components/AdminNavBar.jsx
+++ src/components/AdminNavBar.jsx
@@ -20,26 +20,26 @@ const AdminNavBar = () => {
       <>
         {windowWidth < 700 ? (
           <>
-            <div className="flex mt-2 mb-4 uppercase justify-center text-xs gap-1">
-              <div className="hover:text-blue-400 p-2">
+            <div className="text-center mt-2 mb-4 uppercase justify-center text-xs">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/">Glavna</Link>
               </div>
-              <div className="hover:text-blue-400 p-2">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/admin-panel">Admin</Link>
               </div>
-              <div className="hover:text-blue-400 p-2">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/admin-panel/admin-clanci">Članci</Link>
               </div>
-              <div className="hover:text-blue-400 p-2">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/admin-panel/admin-za-ucenike">Za Učenike</Link>
               </div>
-              <div className="hover:text-blue-400 p-2">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/admin-panel/admin-za-roditelje">Za Roditelje</Link>
               </div>
-              <div className="hover:text-blue-400 p-2">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/admin-panel/admin-uposlenici">Uposlenici</Link>
               </div>
-              <div className="hover:text-blue-400 p-2">
+              <div className="hover:text-blue-400 p-1">
                 <Link to="/admin-panel/admin-o-skoli">O Školi</Link>
               </div>
             </div>
