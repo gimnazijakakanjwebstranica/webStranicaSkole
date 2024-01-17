65f1de32feda5356bcb4a0f2e5dd77e9f2c34f88
mirzareact <hodzic04mirza@gmail.com>
2024-01-17 22:12:50 +0100

    backend radi online, stranica online sve radi i na mobu official

--- backend/config.js
+++ backend/config.js
@@ -3,4 +3,4 @@ export const MONGO_URI =
   "mongodb+srv://render:render@gimnazijakakanj.pg4jtcy.mongodb.net/test";
 export const SALTROUNDS = 10;
 export const SECRET = "dgt44kj";
-export const BACKEND_URL = "http://localhost:5555"
+export const BACKEND_URL = "https://gimnazijamuhsinrizvickakanj.onrender.com";
