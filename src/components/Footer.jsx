258b235bfe8263d9102cde85c31882f659bd4e12
Nedim Neimarlija <nedimnneimarlija@gmail.com>
2024-04-17 10:55:00 +0200

    footer uredio brate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,38 @@
 import React from "react";
-import { SiGmail } from "react-icons/si";
+import { FaEnvelope } from "react-icons/fa"; // Import FaEnvelope
 
 const Footer = () => {
   return (
-    <footer className=" bottom-0 w-full bg-gray-800 text-white py-4">
+    <footer className="bottom-0 w-full bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white py-8">
       <div className="container mx-auto">
         {/* Kontakt */}
-        <div className="p-4">
+        <div className="p-4 bg-gray-800 rounded-md shadow-md">
           <div className="md:w-full">
-            <h6 className="text-xl font-semibold mb-2 left-1 flex items-center">
+            <h6 className="text-xl font-semibold mb-4 flex items-center">
+              <FaEnvelope className="mr-2" />
               Kontakt
             </h6>
-            <ul className="list-none p-0 m-0 left-1">
+            <ul className="list-none p-0 m-0">
               <li className="flex items-center mb-2">
-                <p className="fas fa-map-marker-alt mr-2"></p>
-                <address>
-                  Ulica šehida 32, Kakanj <br />
-                  Kakanj, 72240
-                </address>
+                <p className="mr-2">Ulica šehida 32, Kakanj</p>
+                <p>Kakanj, 72240</p>
               </li>
               <li className="flex items-center mb-2">
-                <i className="fas fa-phone mr-2"></i> 032 456 241
+                <p className="mr-2">032 456 241</p>
               </li>
               <li className="flex items-center">
-                <i className="far fa-envelope mr-2"></i>
-                gimnazijakakanj@bih.net.ba
+                <p className="mr-2">gimnazijakakanj@bih.net.ba</p>
               </li>
             </ul>
           </div>
         </div>
 
         {/* Copyright */}
-        <div className="container mx-auto text-center mt-4">
-          <p className="text-sm">
+        <div className="container mx-auto text-center mt-6">
+          <p className="text-lg">
             &copy; 2024 - Sva prava zadržana -{" "}
-            <a href="https://www.hodzicmirza.com" className="underline pr-1">
-              Mirza Hodzic
+            <a href="https://www.hodzicmirza.com" className="underline pr-4">
+              Mirza Hodžić
             </a>
             <a href="https://www.facebook.com/nedimnn10" className="underline">
               Nedim Neimarlija
